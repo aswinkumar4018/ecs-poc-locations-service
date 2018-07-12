3974f4b985d939fb7b36094bc9046ca992f5f333
Aswin Kumar <aswin.kumar@autodesk.com>
2018-07-12 19:14:04 +0530

    test

--- server.js
+++ server.js
@@ -1,30 +1,44 @@
 const app = require('koa')();
 const router = require('koa-router')();
 const db = require('./db.json');
+const request = require('request');
 
 // Log requests
-app.use(function *(next) {
-  const start = new Date;
-  yield next;
-  const ms = new Date - start;
-  console.log('%s %s - %s', this.method, this.url, ms);
+app.use(function*(next) {
+    const start = new Date;
+    yield next;
+    const ms = new Date - start;
+    console.log('%s %s - %s', this.method, this.url, ms);
 });
 
-router.get('/api/locations', function *() {
-  this.body = db.locations;
+router.get('/api/locations', function*() {
+    this.body = db.locations;
 });
 
-router.get('/api/locations/:id', function *() {
-  const id = parseInt(this.params.id, 10);
-  this.body = db.locations.find((location) => location.id === id);
+async function test() {
+    return await request('http://characters/api/characters', { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log(body);
+        return "body";
+    });
+}
+
+router.get('/api/locations/getAllCharacters', function*() {
+    this.body = yield test();
 });
 
-router.get('/api/', function *() {
-  this.body = 'API ready to receive requests';
+
+router.get('/api/locations/:id', function*() {
+    const id = parseInt(this.params.id, 10);
+    this.body = db.locations.find((location) => location.id === id);
 });
 
-router.get('/', function *() {
-  this.body = 'Ready to receive requests';
+router.get('/api/', function*() {
+    this.body = 'API ready to receive requests';
+});
+
+router.get('/', function*() {
+    this.body = 'Ready to receive requests';
 });
 
 app.use(router.routes());
@@ -33,6 +47,6 @@ app.use(router.allowedMethods());
 var server = app.listen(80);
 
 process.on('SIGTERM', function() {
-  console.log('Shutting down...');
-  server.close();
+    console.log('Shutting down...');
+    server.close();
 });
